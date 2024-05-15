2253eb2c31c3fe796d6c8e77f1db30b76d39cfbf
Nicolas Horton <nic.horton00@gmail.com>
2024-05-15 10:04:38 -0500

    Restructured folders

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
-import Image from 'next/image';
-import HeroSection from './components/HeroSection';
+import HeroSection from './components/HeroSection/HeroSection';
 
 export default function Home() {
 	return (
