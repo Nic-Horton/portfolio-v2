fa8860ac78a6668cf87db8d082ad5969c061fadd
Nicolas Horton <nic.horton00@gmail.com>
2024-05-15 23:26:25 -0500

    Added animations to hero section

--- app/components/HeroSection/TypeAnimation.jsx
+++ app/components/HeroSection/TypeAnimation.jsx
@@ -1,13 +1,12 @@
-'use client';
 import React from 'react';
 import { TypeAnimation as Animation } from 'react-type-animation';
 
 const TypeAnimation = () => {
 	return (
 		<Animation
-			sequence={['Nic', 2000, 'Software Developer', 2000]}
+			sequence={['', 1300, 'Nic', 3000, 'Software Developer', 2000]}
 			wrapper="span"
-			speed={20}
+			speed={40}
 			repeat={0}
 		/>
 	);
