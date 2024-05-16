6a75b86b79cc1e8be463783b4da888bb8b185f68
Nicolas Horton <nic.horton00@gmail.com>
2024-05-15 23:07:26 -0500

    Implemented color scheme to make color changes easier

--- app/components/AboutSection/TabButton.jsx
+++ app/components/AboutSection/TabButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TabButton = ({ active, selectTab, children }) => {
 	const buttonClasses = active
-		? 'text-white border-b border-red-500'
+		? 'text-white border-b border-primary-500'
 		: 'text-[#ADB7BE] ';
 
 	return (
--- app/components/HeroSection/HeroSection.jsx
+++ app/components/HeroSection/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
 			<div className="grid grid-cols-1 sm:grid-cols-12">
 				<div className="col-span-7 place-self-center text-center sm:text-left">
 					<h1 className="text-white mb-4 text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-extrabold">
-						<span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-orange-600">
+						<span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-600 to-secondary-500">
 							Hello, I&apos;m{' '}
 						</span>
 						<br />
@@ -19,10 +19,10 @@ const HeroSection = () => {
 						amet nobis laboriosam quisquam ducimus debitis blanditiis vero.
 					</p>
 					<div>
-						<button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-red-500 via-orange-500 to-yellow-500 hover:text-slate-200 text-white">
+						<button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-600 via-secondary-500 to-yellow-400 hover:text-slate-200 text-white">
 							Contact me
 						</button>
-						<button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-red-500 via-orange-500 to-yellow-500 hover:bg-slate-800 text-white mt-3">
+						<button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-600 via-secondary-500 to-yellow-400 hover:bg-slate-800 text-white mt-3">
 							<span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
 								Download Resume
 							</span>
