ca915d9ef60ec536b63243c84ef6e4edf7a6d8d1
Nicolas Horton <nic.horton00@gmail.com>
2024-05-16 20:14:01 -0500

    reversed deploy commits

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: 'export',
-	target: 'experimental-serverless-trace',
-};
+const nextConfig = {};
 
 export default nextConfig;
