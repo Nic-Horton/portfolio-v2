030ed89fdbbc9876cc974b085a8844c931815852
Nicolas Horton <nic.horton00@gmail.com>
2024-05-16 19:59:50 -0500

    Changed configs for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: 'export',
+	target: 'experimental-serverless-trace',
+};
 
 export default nextConfig;
