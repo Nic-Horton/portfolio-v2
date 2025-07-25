d0d6b5926f2b580ea05887685d2057f8b783194d
Nicolas Horton <nic.horton00@gmail.com>
2025-07-25 17:51:37 -0500

    Changed Typing animation

--- app/components/HeroSection/TypeAnimation.jsx
+++ app/components/HeroSection/TypeAnimation.jsx
@@ -4,7 +4,17 @@ import { TypeAnimation as Animation } from 'react-type-animation';
 const TypeAnimation = () => {
 	return (
 		<Animation
-			sequence={['', 1300, 'Nic', 1500, 'Software Developer', 1500, 'Nic']}
+			sequence={[
+				'',
+				1300,
+				'Nic',
+				1500,
+				'Software Developer',
+				1500,
+				'Electrical Engineer',
+				1500,
+				'Nic',
+			]}
 			wrapper="span"
 			speed={50}
 			repeat={0}
